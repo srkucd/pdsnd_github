bc52c37ad831fa67ff8961c7668367ae459695f1
srkucd <srkwhu@gmail.com>
2020-03-03 16:36:57 +0000

    fix: Change some incorrect output
    To be honest, the output is not very incorrect, I just want to finish
    this submission.

--- bikeshare.py
+++ bikeshare.py
@@ -262,7 +262,7 @@ def main():
         user_stats(df)
         raw_data(df)
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\nWould you like to restart? yes/no.\n')
         if restart.lower() != 'yes':
             break
 
